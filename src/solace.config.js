abede9ced262cefada7c1d54426e20b430a10993
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-07-02 12:17:28 -0400

    Change Solace hostUrl to WSS

--- src/solace.config.js
+++ src/solace.config.js
@@ -1,7 +1,7 @@
 // SCDS service
 // SOLACE_MQTT_HOST_URL: "wss://mr17km0laaj0b.messaging.solace.cloud:8443",
 export const solaceConfig = {
-  SOLACE_HOST_URL: "ws://mr17km0laaj0b.messaging.solace.cloud:80",
+  SOLACE_HOST_URL: "wss://mr17km0laaj0b.messaging.solace.cloud:443",
   SOLACE_MESSAGE_VPN: "scds",
   SOLACE_USERNAME: "solace-cloud-client",
   SOLACE_PASSWORD: "9s866156nmb5pra5ijab28dgac",
