ff46e21aa97b7ca587b5c468f532361e9513dcb9
Andrew Roberts <andrew.jacob.roberts@gmail.com>
2020-07-02 11:49:31 -0400

    Public facing service connect details added

---
+++ src/solace.config.js
@@ -0,0 +1,14 @@
+// SCDS service
+// SOLACE_MQTT_HOST_URL: "wss://mr17km0laaj0b.messaging.solace.cloud:8443",
+export const solaceConfig = {
+  SOLACE_HOST_URL: "ws://mr17km0laaj0b.messaging.solace.cloud:80",
+  SOLACE_MESSAGE_VPN: "scds",
+  SOLACE_USERNAME: "solace-cloud-client",
+  SOLACE_PASSWORD: "9s866156nmb5pra5ijab28dgac",
+};
+// export const solaceConfig = {
+//   SOLACE_HOST_URL: "ws://localhost:80",
+//   SOLACE_MESSAGE_VPN: "default",
+//   SOLACE_USERNAME: "default",
+//   SOLACE_PASSWORD: "",
+// };
